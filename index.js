67a8fa456c745f384c2b4d40156c09334e76ae0f
Tanmay Khandelwal <iamtanmaykh07@gmail.com>
2023-06-04 11:14:05 +0530

    js practice code

---
+++ index.js
@@ -0,0 +1,22 @@
+// First Practice
+
+// var myName = "Tanmay";
+// console.log(myName)
+
+// var myAge = 26
+// console.log(myAge)
+
+
+// opretor
+// console.log(typeof(myName))
+
+// Expression and Operator
+
+var x = 5;
+var y = 5;
+
+// console.log("is both equal"+ x == y);
+
+
+// Arthmatic Operator
+
